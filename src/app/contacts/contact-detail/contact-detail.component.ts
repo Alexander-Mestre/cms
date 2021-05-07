da160f1fafc58f929bbb20ca6ac340e640ec97ac
Alexander-Mestre <59705436+Alexander-Mestre@users.noreply.github.com>
2021-05-07 14:39:12 -0600

    w02 update

--- src/app/contacts/contact-detail/contact-detail.component.ts
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'app-contact-detail',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactDetailComponent implements OnInit {
 
-  constructor() { }
+  //public contactCard: Contact = new Contact();
+  
+  constructor() {
+   
+   }
 
   ngOnInit(): void {
   }
--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'app-contact-list',
@@ -7,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactListComponent implements OnInit {
 
-  constructor() { }
+  contactList: Contact[] = [
+    new Contact(
+      1
+      ,  "R. Kent Jackson"
+      ,  "jacksonk@byui.edu"
+      ,  "208-496-3771"
+      ,  "/assets/images/jacksonk.jpg"
+      ,  []
+      )
+      ,
+      new Contact(
+       2
+      ,  "Rex Barzee"
+      ,  "barzeer@byui.edu"
+      ,  "208-496-3768"
+      ,  "/assets/images/barzeer.jpg"
+      ,  [])
+  ]
+
+
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
   }
