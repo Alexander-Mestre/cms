90d8eeed971b86976f8d4e8831e423e417955e3c
Alexander-Mestre <59705436+Alexander-Mestre@users.noreply.github.com>
2021-05-08 20:54:47 -0600

    3 till header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  selectedFeature: string = 'contacts';
+
   title = 'cms';
+
+
+  switchView(feature: string) {
+    this.selectedFeature = feature;
+  }
 }
